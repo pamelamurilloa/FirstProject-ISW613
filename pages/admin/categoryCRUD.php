aa5f7e655042ce17f0c6a149e0c3faf6e8cb1600
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-11-06 16:19:38 -0600

    Basic HTML and PHP version1 for categoryCRUD

--- pages/admin/categoryCRUD.php
+++ pages/admin/categoryCRUD.php
@@ -1,3 +1,9 @@
+<?php
+    include('../../scripts/utils/newsManager.php');
+
+    $categorySelected = (isset($_GET['id'])) ? $_GET['id'] : null;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +12,51 @@
     <title>Category Administrator</title>
 </head>
 <body>
-    
+    <div class="CRUD">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Category Name</th>
+                    <td>Edit</td>
+                    <td>Delete</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($categories as $category) : ?>
+                    <tr>
+                        <td><?php echo $category['id']; ?></td>
+                        <td><?php echo $category['name']; ?></td>
+                        <?php
+                        echo '<td><a href="categoryCRUD.php?id=' . $category['id'] . '">Edit</a> </td>';
+                        echo '<td><a href="../../scripts/categories/deleteCategory.php?id=' . $category['id'] . '">Delete</a></td>';
+                        ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <?php 
+            if (isset($_GET['id'])) {
+                echo '<form action="../../scripts/categories/editCategory.php" method="POST" class="form-inline" role="form">';
+            } else {
+                echo '<form action="../../scripts/categories/addCategory.php" method="POST" class="form-inline" role="form">';
+            }
+        ?>
+        
+        <div class="form-group">
+            <label class="label-form" for="name">Category</label>
+            <input type="text" class="form-control" name="name" placeholder="Category name">
+        </div>
+
+        <?php 
+            echo '<input type="hidden" name="categoryID" value="' . $categorySelected . '">';
+        ?>
+
+        <input type="submit" class="btn btn-primary" value="Edit Category"></input>
+
+        <a href="categoryCRUD.php">Stop Editing</a>
+    </div>
 </body>
 </html>
