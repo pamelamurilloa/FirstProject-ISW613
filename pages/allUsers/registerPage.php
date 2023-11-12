6b6d9f35a3dcd531e2ed833e41c4278e3c5638d5
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-11-12 17:41:50 -0600

    Country selection deleted, form type changed and all spaces are now required

--- pages/allUsers/registerPage.php
+++ pages/allUsers/registerPage.php
@@ -1,3 +1,9 @@
+<?php
+    require_once('../../scripts/utils/session/validateSession.php');
+
+  confirmLogin();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,43 +27,32 @@
 
       <div class="form-group">
         <label for="last-name">Last Name</label>
-        <input id="last-name" class="form-control" type="text" name="lastName">
+        <input id="last-name" class="form-control" type="text" name="lastName" required>
       </div>
 
       <div class="form-group">
         <label for="user-name">Username</label>
-        <input id="user-name" class="form-control" type="text" name="userName">
+        <input id="user-name" class="form-control" type="text" name="userName" required>
       </div>
 
       <div class="form-group">
         <label for="password">Password</label>
-        <input id="password" class="form-control" type="password" name="password">
+        <input id="password" class="form-control" type="password" name="password" required>
       </div>
 
       <div class="form-group">
         <label for="passworConfirm">Confirm Password</label>
-        <input id="passworConfirm" class="form-control" type="password" name="passworConfirm">
+        <input id="passworConfirm" class="form-control" type="password" name="passworConfirm" required>
       </div>
 
       <div class="form-group">
         <label for="email">Email Address</label>
-        <input id="email" class="form-control" type="email" name="email">
+        <input id="email" class="form-control" type="email" name="email" required>
       </div>
 
       <div class="form-group">
         <label for="cellphone">Cellphone Number</label>
-        <input id="cellphone" class="form-control" type="text" name="cellphone">
-      </div>
-
-      <div class="form-group">
-        <label for="country">Country</label>
-        <select id="country" class="form-control" name="country">
-          <?php
-          foreach($countries as $id => $country) {
-            echo "<option value=\"$id\">$country</option>";
-          }
-          ?>
-        </select>
+        <input id="cellphone" class="form-control" type="tel" pattern="[0-9]{8}" name="cellphone" placeholder="8 digit phone number" required >
       </div>
 
       <div class="register-options">
