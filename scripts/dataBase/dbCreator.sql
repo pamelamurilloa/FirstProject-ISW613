8df5d51524ada6f7e8578fcb3c3498ef83c53aa8
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-11-11 18:52:11 -0600

    Database creation script completed

---
+++ scripts/dataBase/dbCreator.sql
@@ -0,0 +1,53 @@
+-- DATABASE CREATION
+DROP DATABASE IF EXISTS my_cover;
+CREATE DATABASE my_cover;
+
+-- DATABASE SELECTION
+USE my_cover;
+
+-- TABLE CREATION
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    cellphone INT NOT NULL,
+    country VARCHAR(5) NOT NULL,
+    fk_role_id INT DEFAULT 2,
+    FOREIGN KEY (fk_role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE news_sources (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    url VARCHAR NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    fk_category_id INT,
+    fk_user_id INT,
+    FOREIGN KEY (fk_category_id) REFERENCES categories(id),
+    FOREIGN KEY (fk_user_id) REFERENCES users(id)
+);
+
+CREATE TABLE news (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    short_description VARCHAR(200) NOT NULL,
+    permalink VARCHAR NOT NULL,
+    date DATE NOT NULL,
+    fk_news_sources_id INT,
+    fk_user_id INT,
+    fk_category_id INT,
+    FOREIGN KEY (fk_news_sources_id) REFERENCES news_sources(id),
+    FOREIGN KEY (fk_user_id) REFERENCES users(id),
+    FOREIGN KEY (fk_category_id) REFERENCES categories(id)
+);
