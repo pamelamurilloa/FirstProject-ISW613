ee7ae0d66623dac456bcdd136aebe089e3a5ff35
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-11-12 13:57:28 -0600

    HTML crud base for news sources

--- pages/allUsers/newsSources.php
+++ pages/allUsers/newsSources.php
@@ -1,3 +1,11 @@
+<?php
+    require_once('../../scripts/news/newsManager.php');
+
+    $newsSourceSelected = (isset($_GET['id'])) ? $_GET['id'] : null;
+    $newsSources = getSources();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +14,56 @@
     <title>News Sources</title>
 </head>
 <body>
-    
+    <div class="CRUD">
+
+        <table class="table">
+
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>News Source Name</th>
+                    <td>url</td>
+                    <td>Category</td>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                <?php foreach ($newsSources as $newsSource) :
+                    echo '<tr>';
+
+                        echo '<td>' . $newsSource['id'] . '</td>';
+                        echo '<td>' . $newsSource['name'] . '</td>';
+                        echo '<td>' . $newsSource['url'] . '</td>';
+                        echo '<td>' . $newsSource['category'] . '</td>';
+
+                        echo '<td><a href="newsSources.php?id=' . $newsSource['id'] . '">Edit</a> </td>';
+                        echo '<td><a href="../../scripts/news/deleteNewsSource.php?id=' . $newsSource['id'] . '">Delete</a></td>';
+                        
+                    echo '</tr>';
+                endforeach; ?>
+
+            </tbody>
+        </table>
+
+        <?php 
+            if (isset($_GET['id'])) {
+                echo '<form action="../../scripts/news/editNewsSource.php" method="POST" class="form-inline" role="form">';
+            } else {
+                echo '<form action="../../scripts/news/addNewsSource.php" method="POST" class="form-inline" role="form">';
+            }
+        ?>
+        
+        <div class="form-group">
+            <label class="label-form" for="name">News Source</label>
+            <input type="text" class="form-control" name="name" placeholder="News Source Name">
+        </div>
+
+        <?php echo '<input type="hidden" name="newsSourceID" value="' . $newsSourceSelected . '">'; ?>
+
+        <input type="submit" class="btn btn-primary" value="Edit News Source"></input>
+
+        <a href="newsSources.php">Stop Editing</a>
+    </div>
 </body>
 </html>
--- scripts/news/newsManager.php
+++ scripts/news/newsManager.php
@@ -4,3 +4,8 @@ function getNewsByCategory () {
     $news = array();
     return $news;
 }
+
+function getSources () {
+    $newsSources = array();
+    return $newsSources;
+}
