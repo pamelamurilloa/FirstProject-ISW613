64bca88dda83d3d41335446590c8e90ffb26a2ff
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-11-12 18:35:06 -0600

    Identation added

--- scripts/users/addUser.php
+++ scripts/users/addUser.php
@@ -1,21 +1,21 @@
 <?php
 
-include_once('usersManager.php');
+    include_once('usersManager.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $user['firstName'] = $_POST["firstName"];
-    $user['lastName'] = $_POST['lastName'];
-    $user['password'] = $_POST['password'];
-    $user['passwordConfirm'] = $_POST['passwordConfirm'];
-    $user['userName'] = $_POST['userName'];
-    $user['email'] = $_POST['email'];
-    $user['cellphone'] = $_POST['cellphone'];
+        $user['firstName'] = $_POST["firstName"];
+        $user['lastName'] = $_POST['lastName'];
+        $user['password'] = $_POST['password'];
+        $user['passwordConfirm'] = $_POST['passwordConfirm'];
+        $user['userName'] = $_POST['userName'];
+        $user['email'] = $_POST['email'];
+        $user['cellphone'] = $_POST['cellphone'];
 
-  if ($saveUser($user)) {
-    header("Location: ../../pages/allUsers/loginPage.php");
-  } else {
-    header("Location: ../../pages/allUsers/registerPage.php?error=unable-to-register");
-  }
+    if ($saveUser($user)) {
+        header("Location: ../../pages/allUsers/loginPage.php");
+    } else {
+        header("Location: ../../pages/allUsers/registerPage.php?error=unable-to-register");
+    }
 
-}
+    }
